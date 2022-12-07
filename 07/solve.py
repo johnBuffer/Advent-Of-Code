d0f6486bf5fbb5685424721bf1425c43ba850863
Jean Tampon <jean.tampon@gmail.com>
2022-12-07 13:33:31 +0100

    Add day 07

--- 07/solve.py
+++ 07/solve.py
@@ -3,9 +3,8 @@ data = [l[5:][:-1] if '$' in l[0] else int(l.split(' ')[0]) for l in open('data.
 def build(i):
     subs, size = [], 0
     while i < len(data):
-        if   data[i] == '..'         : return i + 1, [[subs, size]], size
-        elif isinstance(data[i], int): i, size = i + 1, size + data[i]
-        else                         : i, subs, size = [a + b for a, b in zip((0, subs, size), build(i + 1))]
+        if data[i] == '..': return i+1, [[subs, size]], size
+        else: i, subs, size = (i+1, subs, size + data[i]) if type(data[i]) == int else [a+b for a, b in zip((0, subs, size), build(i+1))]
     return len(data), [[subs, size]], size
 
 def find_small(d): return sum(find_small(s) for s in d[0]) + d[1] * (d[1] < 100000)
