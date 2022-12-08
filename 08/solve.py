b2f38bc4023892bc9623447e04e5e62d54bdfda6
Jean Tampon <jean.tampon@gmail.com>
2022-12-08 10:30:17 +0100

    Denser day 08

--- 08/solve.py
+++ 08/solve.py
@@ -1,70 +1,8 @@
 d = [[int(c) for c in l.strip()] for l in open('data.txt')]
-w, h = len(d[0]), len(d)
+w, h, t = len(d[0]), len(d), list(zip(*d))
 
-visible = 0
-for x in range(len(d[0])):
-    for y in range(len(d)):
-        current = d[y][x]
+def is_visible(x, y): return any(d[y][x] > v for v in [max(d[y][:x]), max(d[y][x+1:]), max(t[x][:y]), max(t[x][y+1:])])
+def sup_sum(x, l): return sum(l[x] > max(l[i:x]) for i in range(x)) * (1 + sum(l[x] > max(l[x+1:i]) for i in range(x+2, w)))
 
-        if (x == 0 or y == 0 or x == len(d[0]) - 1 or y == len(d) -1):
-            visible += 1
-        else:
-            left = max(d[y][:x]) < current
-            right = max(d[y][x+1:]) < current
-            top   = max(d[i][x] for i in range(0, y)) < current
-            bottom   = max(d[i][x] for i in range(y + 1, len(d))) < current
-            visible += left or right or top or bottom
-
-
-best = 0
-for x in range(len(d[0])):
-    for y in range(len(d)):
-        current = d[y][x]
-
-        if (x == 0 or y == 0 or x == len(d[0]) - 1 or y == len(d) -1):
-            pass
-        else:
-            left = 0
-            for xx in range(x-1, -1, -1):
-                if d[y][xx] < current:
-                    left += 1
-                if d[y][xx] >= current:
-                    left += 1
-                    break
-
-            right = 0
-            for xx in range(x+1, len(d[0])):
-                if d[y][xx] < current:
-                    right += 1
-                if d[y][xx] >= current:
-                    right += 1
-                    break
-            
-            top = 0
-            for yy in range(y-1, -1, -1):
-                if d[yy][x] < current:
-                    top += 1
-                if d[yy][x] >= current:
-                    top += 1
-                    break
-
-            down = 0
-            for yy in range(y+1, len(d)):
-                if d[yy][x] < current:
-                    down += 1
-                if d[yy][x] >= current:
-                    down += 1
-                    break
-
-            if x == 2 and y == 3:
-                print(current, down, top, left, right)
-
-            best = max(best, down * top * left * right)
-
-
-print(visible)
-# print(2*(w+h)-4 + sum(any(d[y][x] > v for v in [max(d[y][:x]), max(d[y][x+1:]), max(d[i][x] for i in range(y)), max(d[i][x] for i in range(y + 1, h))]) for y in range(1, h-1) for x in range(1, w-1)))
-
-
-
-print(best)
+print(2*(w+h)-4 + sum(is_visible(x, y) for y in range(1, h-1) for x in range(1, w-1)))
+print(max(sup_sum(x, d[y]) * sup_sum(y, t[x]) for y in range(1, h-1) for x in range(1, w-1)))
