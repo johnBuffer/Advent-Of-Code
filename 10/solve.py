39d42d6e44077799a60f18d398d7aa71fa3836fb
Jean Tampon <jean.tampon@gmail.com>
2022-12-10 12:36:38 +0100

    Add day 10

--- 10/solve.py
+++ 10/solve.py
@@ -1,5 +1,5 @@
-data = [0 if p in ['addx', 'noop'] else int(p) for l in open('data.txt') for p in l.strip().split()]
-def xs(i, r, c=1): return xs(i[1:], r + [(r[-1][0] + i[0], c, c%40)], c + 1) if i else r
+data, s = [0 if p in ['addx', 'noop'] else int(p) for l in open('data.txt') for p in l.strip().split()], (1, 0, 0)
+def xs(i, r, c=1): return xs(i[1:], r + [(r[-1][0] + i[0], c+1, c%40)], c + 1) if i else r
 
-print(sum(x * (c + 1) for x, c, m in xs(data, [(1, 0, 0)]) if m == 19))
-print(''.join(('#' if (x-1) <= m <= (x + 1) else ' ') + '\n' * (m == 39) for x, c, m in xs(data, [(1, 0, 0)])))
+print(sum(x * c for x, c, m in xs(data, [s]) if m == 19))
+print(''.join(('#' if (x-1) <= m <= (x + 1) else ' ') + '\n' * (m == 39) for x, _, m in xs(data, [s])))
