0ab55bee39117086b2e1f56bebfcae2c16736bc4
Jean Tampon <jean.tampon@gmail.com>
2022-12-25 12:11:03 +0100

    Add day 25

--- 25/solve.py
+++ 25/solve.py
@@ -4,7 +4,7 @@ def snafu_digit(digit, power): return (-1 if digit == '-' else -2 if digit == '=
 def snafu_to_int(snafu): return snafu_digit(snafu[0], len(snafu)-1) + snafu_to_int(snafu[1:]) if snafu else 0
 def base5(n, p = None): return base5(n, int(math.log(n) / math.log(5))) if p is None else [] if p < 0 else [n // (5**p)] + base5(n % (5**p), p-1)
 def int_to_snafu(number):
-    res = [0]*5 + base5(number)
+    res = [0] + base5(number)
     for i in range(len(res)-1, -1, -1):
         if   res[i] == 3: res[i], res[i-1] = '=', res[i-1] + 1
         elif res[i] == 4: res[i], res[i-1] = '-', res[i-1] + 1
