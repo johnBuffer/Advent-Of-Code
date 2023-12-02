dae580a82f3861e952bd680bce59a4413847a320
Jean Tampon <jean.tampon@intempora.com>
2023-12-02 17:08:12 +0100

    Add day 2

--- 2023/02/solve.py
+++ 2023/02/solve.py
@@ -1,7 +1,7 @@
 import re
 
 def extract(color, game):
-    return [int(re.findall(f'.* (\d+) {color}.*', d)[0]) if color in d else 0 for d in game.split(';')]
+    return [int(*re.findall(f'.* (\d+) {color}.*', d)) for d in game.split(';')]
 games = [[extract(c, l) for c in ['red', 'green', 'blue']] for l in open('data.txt')]
 
 print(sum((k + 1) * all(max(g[i]) <= 12 + i for i in range(3)) for k, g in enumerate(games)))
