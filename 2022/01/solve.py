2ae8a46a93551b6a390fd8668c7413559b1d17b1
Jean Tampon <jean.tampon@gmail.com>
2023-12-04 20:35:51 +0100

    Add day 4

--- 2022/01/solve.py
+++
@@ -1,5 +0,0 @@
-data = [[int(v) for v in l.split('\n')] for l in open('data.txt').read().split('\n\n')]
-sums = sorted([sum(l) for l in data])
-
-print(sums[-1])
-print(sum(sums[-3:]))
--- 2023/04/solve.py
+++ 2023/04/solve.py
@@ -1,5 +1,9 @@
-data = [[[int(x) for x in n.split(' ') if x] for n in p.split(' | ')] for l in open('data.txt') for p in l.strip().split(': ')[1]]
+import re
 
-print(data)
+scores = [35 - len(set(re.findall(r'(\d+)', l)[1:])) for l in open('data.txt')]
 
-#print(2**(sum(x in y) for x, y in ))
+def play(i=0, r={}):
+    return r[i] if i in r else 1 + sum(play(i + x + 1) for x in range(scores[i]))
+
+print(sum(int(2**(x - 1)) for x in scores))
+print(sum(play(i) for i in range(len(scores))))
