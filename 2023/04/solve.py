85cae02be577ea4dda1b11a42c7632da23678986
Jean Tampon <jean.tampon@gmail.com>
2023-12-04 20:56:41 +0100

    Add day 4

--- 2023/04/solve.py
+++ 2023/04/solve.py
@@ -1,9 +1,8 @@
 import re
 
-scores = [35 - len(set(re.findall(r'(\d+)', l)[1:])) for l in open('data.txt')]
-
 def play(i=0, r={}):
     return r[i] if i in r else 1 + sum(play(i + x + 1) for x in range(scores[i]))
 
+scores = [35 - len(set(re.findall(r'(\d+)', l)[1:])) for l in open('data.txt')]
 print(sum(int(2**(x - 1)) for x in scores))
 print(sum(play(i) for i in range(len(scores))))
