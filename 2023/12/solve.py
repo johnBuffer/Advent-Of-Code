d4b50b9f2bed2d13ad0ea56e6a4f89d5ad724fe7
Jean Tampon <jean.tampon@gmail.com>
2023-12-12 16:28:33 +0100

    Add day 12

--- 2023/12/solve.py
+++ 2023/12/solve.py
@@ -1,26 +1,30 @@
-def parse_line(l):
+def parse_line(l, r):
     a, b = l.strip().split()
-    return a, [int(x) for x in b.split(',')]
+    return '?'.join([a] * r), [int(x) for x in b.split(',')] * r
 
-data = [parse_line(l) for l in open('data.txt')]
+def consume(v):
+    return [x - (i==0) for i, x in enumerate(v)]
 
-def is_valid(a, b):
-    p = [x for x in a.split('.') if x]
-    return len(p) == len(b) and all(len(s) == c for s, c in zip(p, b))
-
-def get_possible(a, v, c='', count = 0):
-    if a == '':
-        return count + is_valid(c, v)
-    else:
-        h, r = a[0], a[1:]
-        if h == '?':
-            return get_possible(r, v, c + '.', count) + get_possible(r, v, c + '#', count)
+def solve(line, valid, histo, count=0, last=''):
+    key = line, '_'.join(str(v) for v in valid)
+    if key not in histo:
+        if not line:
+            histo[key] = count + (sum(valid) == 0)
         else:
-            return get_possible(r, v, c + h, count)
+            h, r = line[0], line[1:]
+            if len(valid) > 0:
+                if h == '#':
+                    histo[key] = solve(r, consume(valid), histo, count, h) if valid[0] else count
+                elif h == '.':
+                    if last not in ['', '.']: # Entering a '.' section
+                        histo[key] = count if valid[0] else solve(r, valid[1:], histo, count, h)
+                    else: # Walking a '.' section
+                        histo[key] = solve(r, valid, histo, count, h)
+                elif h == '?':
+                    return solve('.' + r, valid, histo, count, last) + solve('#' + r, valid, histo, count, last)
+            else:
+                histo[key] = count + ('#' not in line)
+    return histo[key]
 
-print(sum(get_possible(a, v) for a, v in data))
-
-data_2 = [('?'.join([a] * 5), 5 * b) for a, b in data]
-for l in data_2:
-    print(l)
-
+print(sum(solve(a, b, {}) for a, b in [parse_line(l, 1) for l in open('data.txt')]))
+print(sum(solve(a, b, {}) for a, b in [parse_line(l, 5) for l in open('data.txt')]))
