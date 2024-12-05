c53197a413f90ef6c1ca832f40682177890d3961
Jean Tampon <jean.tampon@gmail.com>
2024-12-05 11:50:08 +0100

    Add day 5

--- 2024/05/solve.py
+++ 2024/05/solve.py
@@ -1,12 +1,12 @@
 ri, ui = open('input.txt').read().split('\n\n')
 rules, updates = [[int(x) for x in l.split('|')] for l in ri.split('\n')], [tuple(map(int, l.split(','))) for l in ui.split('\n')]
-cond, cache = {n: {a for a, b in rules if b == n} for n in set(x for u in updates for x in u)}, {}
+cond, cache = {(n, u): {a for a, b in rules if b == n and a in u} for u in updates for n in u}, {}
 
 def rank(n, u):
-    return cache.get((n, u)) or cache.update({(n, u): max([rank(x, u) for x in cond[n] if x in u] + [-1]) + 1}) or cache[(n, u)]
+    return cache.get(n) or cache.update({n: max([rank((x, u), u) for x in cond[n]] + [0]) + 1}) or cache[n]
 
 def sort(u):
-    return tuple(sorted(u, key=lambda n: rank(n, u)))
+    return tuple(sorted(u, key=lambda n: rank((n, u), u)))
 
 print(sum(u[len(u)//2] for u in updates if u == sort(u)))
 print(sum(sort(u)[len(u)//2] for u in updates if u != sort(u)))
