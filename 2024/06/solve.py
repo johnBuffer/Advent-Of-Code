3d5c29d719881e8ce87ff91ea12a3a0707f5cfef
Jean Tampon <jean.tampon@gmail.com>
2024-12-06 10:16:10 +0100

    Add day 6

---
+++ 2024/06/solve.py
@@ -0,0 +1,20 @@
+raw = [l.strip() for l in open('input')]
+data = {(x, y): c for y, l in enumerate(raw) for x, c in enumerate(l)}
+start = [(l.index('^'), y) for y, l in enumerate(raw) if '^' in l][0]
+
+def predict(p, w):
+    h, d = set(), (0, -1)
+    while True:
+        if (p, d) in h:
+            return 1, 0
+        h.add((p, d))
+        nxt = (p[0] + d[0], p[1] + d[1])
+        if not nxt in w:
+            return 0, 0#len(set(p for p, _ in h))
+        elif w[nxt] == '#':
+            d = (-d[1], d[0])
+        else:
+            p = nxt
+
+#print(predict(start, data)[1])
+print(sum(predict(start, {pp: '#' if pp == p else cc for pp, cc in data.items()})[0] for p, c in data.items() if c != '#' and p != start))
