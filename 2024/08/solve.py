3effdb2c43e27abc8715da1fc4fa9a69ecb37421
Jean Tampon <jean.tampon@gmail.com>
2024-12-08 08:28:08 +0100

    Add day 8

---
+++ 2024/08/solve.py
@@ -0,0 +1,24 @@
+import math
+data = {(x, y): c for y, l in enumerate(open('input')) for x, c in enumerate(l.strip())}
+
+def get_normalized(x, y):
+    d = math.gcd(x, y)
+    return x // d, y // d
+
+def is_antinode(x, y, s):
+    return any(s.get((2*a-x, 2*b-y)) == c for (a, b), c in s.items() if (a, b) != (x, y))
+
+def is_antinode_2(x, y, s):
+    for (a, b), c in s.items():
+        if (a, b) == (x, y):
+            return 1
+        vx, vy = get_normalized(a - x, b - y)
+        if any(s.get((a+i*vx, b+i*vy)) == c for i in range(1, 50)):
+            return 1
+    return 0
+
+def is_antinode_3(x, y, s):
+    return any((a, b) == (x, y) or for (a, b), c in s.items())
+
+print(sum(is_antinode(x, y, {p: c for p, c in data.items() if c != '.'}) for x, y in data))
+print(sum(is_antinode_2(x, y, {p: c for p, c in data.items() if c != '.'}) for x, y in data))
