8e4da51dcd5a0bb0de54823e9fd249f643b59893
Jean Tampon <jean.tampon@gmail.com>
2024-12-11 10:17:28 +0100

    Add day 11

--- 2024/11/solve.py
+++ 2024/11/solve.py
@@ -3,11 +3,14 @@ data, cache = [int(x) for x in open('input').read().strip().split(' ')], {}
 def split(s):
     return [int(p) for p in [s[:len(s)//2], s[len(s)//2:]]]
 
+def mut(x):
+    return [1] if not x else split(str(x)) if len(str(x)) % 2 == 0 else [x*2024]
+
 def get(k):
     return cache.get(k) or cache.update({k: length(*k)}) or cache[k]
 
 def length(x, i):
-    return cache.get((x, i)) or 1 if not i else get((1, i-1)) if not x else sum(get((a, i-1)) for a in split(str(x))) if len(str(x)) % 2 == 0 else get((x * 2024, i-1))
+    return cache.get((x, i)) or 1 if not i else sum(get((n, i-1)) for n in mut(x))
 
 print(sum(length(x, 25) for x in data))
 print(sum(length(x, 75) for x in data))
