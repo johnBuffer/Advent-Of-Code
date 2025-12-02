3e686ba90e224431cc0ec767fb44e3bb384ede58
Jean Tampon <jean.tampon@gmail.com>
2025-12-02 19:51:46 +0100

    KOMPAKT

--- 2025/02/solve.py
+++ 2025/02/solve.py
@@ -1,9 +1,4 @@
 import re
 data = [[int(n) for n in p.split('-')] for p in open('input.txt').read().strip().split(',')]
-
-pattern_1, pattern_2 = re.compile(r'^(.+)\1$'), re.compile(r'^(.+)\1+$')
-def is_invalid_re(n, part_2):
-    return re.match(pattern_2 if part_2 else pattern_1, str(n))
-
-print(sum(n for a, b in data for n in range(a, b+1) if is_invalid_re(n, False)))
-print(sum(n for a, b in data for n in range(a, b+1) if is_invalid_re(n, True)))
+print(sum(n for a, b in data for n in range(a, b+1) if re.match(r'^(.+)\1$', str(n))))
+print(sum(n for a, b in data for n in range(a, b+1) if re.match(r'^(.+)\1+$', str(n))))
