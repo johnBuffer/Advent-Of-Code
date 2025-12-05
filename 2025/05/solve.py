801bd717a71aa0face339cb22129f8b2cd8987da
Jean Tampon <jean.tampon@gmail.com>
2025-12-05 12:31:27 +0100

    Add day 5

--- 2025/05/solve.py
+++ 2025/05/solve.py
@@ -1,23 +1,16 @@
 p1, p2 = open('input.txt').read().split('\n\n')
-ranges = [[int(s) for s in r.split('-')] for r in p1.split('\n')]
+ranges = sorted([[int(s) for s in r.split('-')] for r in p1.split('\n')], key=lambda x: x[0])
 ids = [int(i) for i in p2.split('\n')]
 
-ranges = sorted(ranges, key=lambda x: x[0])
 merged = [ranges[0]]
 last = ranges[0][1]
 for i in range(1, len(ranges)):
     a, b = ranges[i]
-    if a <= last:
-        if b > last:
-            last = b
-            merged[-1][1] = b
-        else:
-            # Nothing to do
-            pass
-    else:
-        merged.append([a, b])
+    if a <= last and b > last:
+        last, merged[-1][1] = b, b
+    elif a > last:
+        last, merged = b, merged + [[a, b]]
         last = b
-ranges = merged
 
-print(sum(any(a <= i <= b for a, b in ranges) for i in ids))
+print(sum(any(a <= i <= b for a, b in merged) for i in ids))
 print(sum((b - a + 1) for a, b in merged))
