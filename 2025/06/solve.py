d210d157a3551ba9490992c79a89bb3d935e7369
Jean Tampon <jean.tampon@gmail.com>
2025-12-06 20:18:26 +0100

    Add day 6

--- 2025/06/solve.py
+++ 2025/06/solve.py
@@ -1,12 +1,11 @@
 import math
-
 lines = [l[:-1] for l in open('input.txt')]
-ops, lc, s = [p for p in lines[-1].split(' ') if p], len(lines) - 1, '.'
+ops, lc = [p for p in lines[-1].split(' ') if p], len(lines) - 1
+pos = [i for i, c in enumerate(lines[-1]) if c != ' '] + [len(lines[-1]) + 2]
+groups = [[lines[i][a:b-1] for i in range(lc)] for a, b in zip(pos, pos[1:])]
 
-def solve(c, op): return sum(c) if op == '+' else math.prod(c)
-def get_col(i): return ''.join(lines[k][i] for k in range(lc))
-def is_num(num): return any(c != ' ' for c in num)
+def solve(g, op): return (sum if op == '+' else math.prod)(int(n) for n in g)
+def transpose(g): return [''.join(g[k][i] for k in range(lc)) for i in range(len(g[0]))]
 
-data = [[int(n) for n in p.split(' ') if n] for p in lines[:-1]]
-print(sum(solve([data[k][i] for k in range(lc)], ops[i]) for i in range(len(data[0]))))
-print(sum(solve(c, ops[i]) for i, c in enumerate([int(n) for n in c.split(s) if n] for c in s.join(get_col(i) for i in range(len(lines[0]))).split(' ' * lc))))
+print(sum(solve(*o) for o in zip(groups, ops)))
+print(sum(solve(transpose(g), o) for g, o in zip(groups, ops)))
