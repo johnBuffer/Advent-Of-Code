e76d94da8ca5cef20f14d677a0d53c01909f9ece
Jean Tampon <jean.tampon@gmail.com>
2025-12-06 20:23:54 +0100

    Add day 6

--- 2025/06/solve.py
+++ 2025/06/solve.py
@@ -5,7 +5,6 @@ pos = [i for i, c in enumerate(lines[-1]) if c != ' '] + [len(lines[-1]) + 2]
 groups = [[lines[i][a:b-1] for i in range(lc)] for a, b in zip(pos, pos[1:])]
 
 def solve(g, op): return (sum if op == '+' else math.prod)(int(n) for n in g)
-def transpose(g): return [''.join(g[k][i] for k in range(lc)) for i in range(len(g[0]))]
 
 print(sum(solve(*o) for o in zip(groups, ops)))
-print(sum(solve(transpose(g), o) for g, o in zip(groups, ops)))
+print(sum(solve([''.join(c) for c in zip(*g)], o) for g, o in zip(groups, ops)))
